ab6d15b12a39204e7cd0f96f3bafb6cf36a2d8f5
alekLukanen <alukanen@icloud.com>
2024-07-21 10:46:21 -0500

    remove unused argument

--- arrowOps/take.go
+++ arrowOps/take.go
@@ -37,50 +37,50 @@ func TakeArray(mem *memory.GoAllocator, arr arrow.Array, indices *array.Uint32) 
 	case arrow.BOOL:
 		return takeBoolArray(mem, arr.(*array.Boolean), indices)
 	case arrow.INT8:
-		return takeNativeArray[int8, *array.Int8](mem, array.NewInt8Builder(mem), arr.(*array.Int8), indices)
+		return takeNativeArray[int8, *array.Int8](array.NewInt8Builder(mem), arr.(*array.Int8), indices)
 	case arrow.INT16:
-		return takeNativeArray[int16, *array.Int16](mem, array.NewInt16Builder(mem), arr.(*array.Int16), indices)
+		return takeNativeArray[int16, *array.Int16](array.NewInt16Builder(mem), arr.(*array.Int16), indices)
 	case arrow.INT32:
-		return takeNativeArray[int32, *array.Int32](mem, array.NewInt32Builder(mem), arr.(*array.Int32), indices)
+		return takeNativeArray[int32, *array.Int32](array.NewInt32Builder(mem), arr.(*array.Int32), indices)
 	case arrow.INT64:
-		return takeNativeArray[int64, *array.Int64](mem, array.NewInt64Builder(mem), arr.(*array.Int64), indices)
+		return takeNativeArray[int64, *array.Int64](array.NewInt64Builder(mem), arr.(*array.Int64), indices)
 	case arrow.UINT8:
-		return takeNativeArray[uint8, *array.Uint8](mem, array.NewUint8Builder(mem), arr.(*array.Uint8), indices)
+		return takeNativeArray[uint8, *array.Uint8](array.NewUint8Builder(mem), arr.(*array.Uint8), indices)
 	case arrow.UINT16:
-		return takeNativeArray[uint16, *array.Uint16](mem, array.NewUint16Builder(mem), arr.(*array.Uint16), indices)
+		return takeNativeArray[uint16, *array.Uint16](array.NewUint16Builder(mem), arr.(*array.Uint16), indices)
 	case arrow.UINT32:
-		return takeNativeArray[uint32, *array.Uint32](mem, array.NewUint32Builder(mem), arr.(*array.Uint32), indices)
+		return takeNativeArray[uint32, *array.Uint32](array.NewUint32Builder(mem), arr.(*array.Uint32), indices)
 	case arrow.UINT64:
-		return takeNativeArray[uint64, *array.Uint64](mem, array.NewUint64Builder(mem), arr.(*array.Uint64), indices)
+		return takeNativeArray[uint64, *array.Uint64](array.NewUint64Builder(mem), arr.(*array.Uint64), indices)
 	case arrow.FLOAT16:
-		return takeNativeArray[float16.Num, *array.Float16](mem, array.NewFloat16Builder(mem), arr.(*array.Float16), indices)
+		return takeNativeArray[float16.Num, *array.Float16](array.NewFloat16Builder(mem), arr.(*array.Float16), indices)
 	case arrow.FLOAT32:
-		return takeNativeArray[float32, *array.Float32](mem, array.NewFloat32Builder(mem), arr.(*array.Float32), indices)
+		return takeNativeArray[float32, *array.Float32](array.NewFloat32Builder(mem), arr.(*array.Float32), indices)
 	case arrow.FLOAT64:
-		return takeNativeArray[float64, *array.Float64](mem, array.NewFloat64Builder(mem), arr.(*array.Float64), indices)
+		return takeNativeArray[float64, *array.Float64](array.NewFloat64Builder(mem), arr.(*array.Float64), indices)
 	case arrow.STRING:
-		return takeNativeArray[string, *array.String](mem, array.NewStringBuilder(mem), arr.(*array.String), indices)
+		return takeNativeArray[string, *array.String](array.NewStringBuilder(mem), arr.(*array.String), indices)
 	case arrow.BINARY:
 		return takeBinaryArray(mem, arr.(*array.Binary), indices)
 	case arrow.DATE32:
-		return takeNativeArray[arrow.Date32, *array.Date32](mem, array.NewDate32Builder(mem), arr.(*array.Date32), indices)
+		return takeNativeArray[arrow.Date32, *array.Date32](array.NewDate32Builder(mem), arr.(*array.Date32), indices)
 	case arrow.DATE64:
-		return takeNativeArray[arrow.Date64, *array.Date64](mem, array.NewDate64Builder(mem), arr.(*array.Date64), indices)
+		return takeNativeArray[arrow.Date64, *array.Date64](array.NewDate64Builder(mem), arr.(*array.Date64), indices)
 	case arrow.TIMESTAMP:
 		return takeNativeArray[arrow.Timestamp, *array.Timestamp](
-			mem, array.NewTimestampBuilder(mem, arr.DataType().(*arrow.TimestampType)), arr.(*array.Timestamp), indices,
+			array.NewTimestampBuilder(mem, arr.DataType().(*arrow.TimestampType)), arr.(*array.Timestamp), indices,
 		)
 	case arrow.TIME32:
 		return takeNativeArray[arrow.Time32, *array.Time32](
-			mem, array.NewTime32Builder(mem, arr.DataType().(*arrow.Time32Type)), arr.(*array.Time32), indices,
+			array.NewTime32Builder(mem, arr.DataType().(*arrow.Time32Type)), arr.(*array.Time32), indices,
 		)
 	case arrow.TIME64:
 		return takeNativeArray[arrow.Time64, *array.Time64](
-			mem, array.NewTime64Builder(mem, arr.DataType().(*arrow.Time64Type)), arr.(*array.Time64), indices,
+			array.NewTime64Builder(mem, arr.DataType().(*arrow.Time64Type)), arr.(*array.Time64), indices,
 		)
 	case arrow.DURATION:
 		return takeNativeArray[arrow.Duration, *array.Duration](
-			mem, array.NewDurationBuilder(mem, arr.DataType().(*arrow.DurationType)), arr.(*array.Duration), indices,
+			array.NewDurationBuilder(mem, arr.DataType().(*arrow.DurationType)), arr.(*array.Duration), indices,
 		)
 	default:
 		return nil, ErrUnsupportedDataType
@@ -102,7 +102,7 @@ func takeBoolArray(mem *memory.GoAllocator, arr *array.Boolean, indices *array.U
 	return b.NewBooleanArray(), nil
 }
 
-func takeNativeArray[T comparable, E valueArray[T]](mem *memory.GoAllocator, b arrayBuilder[T], arr E, indices *array.Uint32) (E, error) {
+func takeNativeArray[T comparable, E valueArray[T]](b arrayBuilder[T], arr E, indices *array.Uint32) (E, error) {
 	defer b.Release()
 	arrLen := arr.Len()
 	b.Reserve(indices.Len())
